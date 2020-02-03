98f8650bd1d16b8ba946bddc25b49c99bd96e7d3
Jameel Brice <jameelbrice@gmail.com>
2020-02-02 19:44:42 -0500

    Part 2

---
+++ flixer/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixer
+//
+//  Created by Jameel Brice on 2/2/20.
+//  Copyright © 2020 Jameel. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
